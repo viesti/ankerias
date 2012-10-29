0db194fffdb8f9b7297c3e13f191ef47344e135b
Kimmo Koskinen <kimmo@tiuhti.net>
2012-10-29 08:17:56 +0200

    Read switch state

--- src/Main.hs
+++ src/Main.hs
@@ -8,6 +8,7 @@ import           Snap.Util.FileServe
 import           Snap.Http.Server
 import           System.Process
 import qualified Data.ByteString.Char8 as C
+import           Text.Regex.TDFA
 
 main :: IO ()
 main = quickHttpServe site
@@ -23,15 +24,24 @@ switchHandler = do
   state <- getParam "state"
   case state of Just "on" -> switch On
                 Just "off" -> switch Off
+                Just "state" -> getState
                 _ -> writeBS "error"
 
 data SwitchState = On | Off
 
 switch :: MonadSnap m => SwitchState -> m ()
-switch On = switchAndPrint "--on"
-switch Off = switchAndPrint "--off"
+switch On = switchAndWrite "--on"
+switch Off = switchAndWrite "--off"
 
-switchAndPrint :: MonadSnap m => String -> m ()
-switchAndPrint s = do
-  out <- liftIO (readProcess "tdtool" [s, "1"] "")
+switchAndWrite :: MonadSnap m => String -> m ()
+switchAndWrite s = do
+  (_, out, _) <- liftIO (readProcessWithExitCode "tdtool" [s, "1"] "")
   writeBS (C.pack out)
+
+getState :: MonadSnap m => m ()
+getState = do
+  (_, out, _) <- liftIO (readProcessWithExitCode "tdtool" ["--list"] "")
+  let pat = "1[ ]*bataatti[ ]*(.*)" :: String
+  case out =~ pat :: [[String]] of [[_,"OFF"]] -> writeBS "off"
+                                   [[_,"ON"]]  -> writeBS "on"
+                                   _           -> writeBS "error"
